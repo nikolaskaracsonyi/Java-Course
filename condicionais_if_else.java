6e00e035290cd6fe0f0287d4e6edab0495b4a8d9
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2021-11-30 00:51:10 -0300

    more codes

--- condicionais_if_else.java
+++ condicionais_if_else.java
@@ -1,8 +1,13 @@
-byte idade = 18;
-if(idade < 10){
-    System.out.println("Menor de idade");
-}else if(idade == 18){
-    System.out.println("Tem 18 anos");
-}else{
-    System.out.println("É menor de idade");
+public class condicionais_if_else {
+    public static void main(String[] args){
+        byte idade = 18;
+        if(idade < 18){
+            System.out.println("Menor de idade");
+        }else if (idade == 18){
+            System.out.println("Tem 18 anos");
+        
+        }else{
+            System.out.println("É menor de idade");
+        }
+    }    
 }
